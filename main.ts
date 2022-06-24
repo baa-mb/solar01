47e0c3fd49bed170c4fd3a147116c962c85c3cec
baa <34665120+baa-mb@users.noreply.github.com>
2022-06-24 17:41:21 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 basic.showIcon(IconNames.Yes)
+datalogger.setColumnTitles(
+"L",
+"X"
+)
+let n = 0
 basic.forever(function () {
+    datalogger.log(
+    datalogger.createCV("L", input.lightLevel()),
+    datalogger.createCV("X", input.acceleration(Dimension.Y))
+    )
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(2000)
     basic.showString("SOLAR")
-    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+    n += 1
+    basic.showNumber(n)
 })
